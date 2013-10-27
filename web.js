4a1583d7248f4b84014398493573dc76d52c181b
Povilas Panavas <povilas.panavas@gmail.com>
2013-10-27 16:00:35 +0000

    Added files for node

---
+++ web.js
@@ -0,0 +1,16 @@
+﻿var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var fileBuffer = fs.readFileSync('index.html');
+  var textToDisplay = fileBuffer.toString();  
+  
+  response.send(textToDisplay);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
